0062497ab088fec2cd995280278daa7ec7b520ba
JakobHolst <jakob.holst@knowit.com>
2023-06-09 14:00:34 +0200

    demo

---
+++ create_project.py
@@ -0,0 +1,20 @@
+import requests
+import os
+
+def post_data(url, data):
+    try:
+        response = requests.post(url, json=data)
+        response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
+        print('POST request successful. Response: {}'.format(response.text))
+    except requests.exceptions.RequestException as e:
+        print('Error: {}'.format(str(e)))
+
+
+if __name__ == '__main__':
+    url = os.getenv('RELEASEMANAGER_URL') + '/projects'
+
+
+    data = {
+        'project_name': 'test',
+    }
+    post_data(url, data)
